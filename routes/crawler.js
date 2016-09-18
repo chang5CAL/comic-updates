6ee6d21bf4708e939c8ce9fbe83ac4e41d902f2d
chang5CAL <ginka13@gmail.com>
2016-09-18 01:19:07 -0700

    added the crawler

---
+++ routes/crawler.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+var Models = require('../models');
+
+/* Simple find */
+router.get('/crawlAll', function(req, res, next) {
+	
+});
+
+module.exports = router;
