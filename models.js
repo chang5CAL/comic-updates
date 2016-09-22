a65fb98c8c2cf2cf82912acdbea90bcc337b3ab0
chang5CAL <ginka13@gmail.com>
2016-09-21 23:27:22 -0700

    working on models

--- models.js
+++ models.js
@@ -3,11 +3,33 @@ mongoose.connect("mongodb://localhost/test")
 
 var Schema = mongoose.Schema;
 
-var ComicSiteSchema = new Schema({
+/*TODO*/
+var ComicSchema = new Schema({
   url: {type: String, unique: true},
-  title: {type: String, unique: true}
+  comic_title: {type: String, unique: true}
 });
 
-var ComicSite = mongoose.model("ComicSite", ComicSiteSchema);
+/*TODO*/
+var ComicProfileSchema = new Schema({
+	comic_title: {type: String, unique: true},
+	tags: [String]
+});
 
+/*TODO*/
+var ComicChapter = new Schema({
+	comic_title: {type: String, unique: true},
+	chapter_title: String,
+	chapter: Number,
+	chapter_url: String
+});
+
+var TagSchema = new Schema({
+	tag: {type: String, unique: true},
+	comics: [Schema.Types.ObjectId]
+});
+
+var Comic = mongoose.model("Comic", ComicSchema);
+var ComicProfile = mongoose.model("ComicProfile", ComicSchema);
+var ComicChapters = mongoose.model("ComicChapters", ComicChaptersSchema);
+var Tag = mongoose.model("Tag", TagSchema)
 module.exports.ComicSite = ComicSite;
