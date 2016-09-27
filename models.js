5d799d0d61c419967372d639ecdbe008a85fb326
chang5CAL <ginka13@gmail.com>
2016-09-26 22:12:34 -0700

    finished basic scraping

--- models.js
+++ models.js
@@ -10,13 +10,12 @@ var ComicSchema = new Schema({
   archive_url: String,
   profile_url: String,
   current_chapter: Number,
-  current_chapter_url: String,
   image: String
 });
 
 /*TODO*/
 var ProfileSchema = new Schema({
-	comics_id: {type: Schema.Types.ObjectId, index: true},
+	comic_id: {type: Schema.Types.ObjectId, index: true},
 	comic_title: String,
 	genre: [String],
 	language: String,
@@ -26,12 +25,12 @@ var ProfileSchema = new Schema({
 });
 
 /*TODO*/
-var ChapterSchema = new Schema({
-	comics_id: {type: Schema.Types.ObjectId, index: true},
+var PageSchema = new Schema({
+	comic_id: {type: Schema.Types.ObjectId, index: true},
 	comic_title: String,
-	chapter_title: String,
-	chapter: Number,
-	chapter_url: {type: String, unique: true},
+	page_title: String,
+	page: Number,
+	page_url: {type: String, unique: true},
 	date_added: String
 });
 
@@ -47,12 +46,12 @@ var kittenSchema = new Schema({
 
 var Comic = mongoose.model("Comic", ComicSchema);
 var Profile = mongoose.model("Profile", ProfileSchema);
-var Chapter = mongoose.model("Chapter", ChapterSchema);
+var Page = mongoose.model("Page", PageSchema);
 var Genre = mongoose.model("Genre", GenreSchema)
 var Kitten = mongoose.model("Kitten", kittenSchema);
 
 module.exports.Comic = Comic;
 module.exports.Profile = Profile;
-module.exports.Chapter = Chapter;
+module.exports.Page = Page;
 module.exports.Genre = Genre;
-module.exports.Kitten = kittenSchema;
+module.exports.Kitten = Kitten;
--- routes/scraper.js
+++ routes/scraper.js
@@ -8,54 +8,71 @@ var moment = require('moment');
 
 /* Simple find */
 router.get('/', function(req, res, next) {
-	x('http://comicfury.com/index.php?page=1', {
-		image: ['.fpcomicavatar img@src'],
-		comic_title: ['.fpcomicdescription > a'],
-		url: ['.fpcomicdescription > a@href'],
-		archive_url: ['.comicactions > a:nth-child(2)@href'], // make sure to grab odd numbers (1, 3)
-		profile_url: ['.comicactions > a:nth-child(1)@href'],
-		current_chapter: ['.fpcomicstat:nth-child(1)@html'] // not used directly
-	})(function(err, obj) {
-		console.log(err);
-		console.log("object");
-		console.log(obj.current_chapter[0]);
-		var list = [];
-		res.json(obj);
-		for (var i = 0; i < obj.profile_url.length; i++) {
-			var chapter = obj.current_chapter[i].replace("</b>", "").replace("<b>", "").replace("Comics: ", "");
-			var comic = new Models.Comic({
-				url: obj.url[i],
-				comic_title: obj.comic_title[i],
-				archive_url: obj.archive_url[i],
-				profile_url: obj.profile_url[i],
-				current_chapter: chapter,
-				image: obj.image[i]
-			})
-			
-			console.log(comic);
-			//randomA(i, obj.profile_url[i], obj.comic_title[i], comic._id);
-		}
-	});
+	findComics(1);
+	res.json("Success")
 });
 
-function randomA(index, data, title, id) {
-	console.log("randomA called");
-	console.log(index);
-	console.log(data);
+router.get('/findNew', function(req, res, next) {
+	findComics(50);
+	res.json("Success")
+});
 
-	x('http://comicfury.com/comicprofile.php?url=bentelbows', {
+function findComics(max) {
+	for (var i = 1; i <= max; i++) {
+		x('http://comicfury.com/index.php?page=' + i, {
+			image: ['.fpcomicavatar img@src'],
+			comic_title: ['.fpcomicdescription > a'],
+			url: ['.fpcomicdescription > a@href'],
+			archive_url: ['.comicactions > a:nth-child(2)@href'], // make sure to grab odd numbers (1, 3)
+			profile_url: ['.comicactions > a:nth-child(1)@href'],
+			current_chapter: ['.fpcomicstat:nth-child(1)@html'] // not used directly
+		})(function(err, obj) {
+			if (!err) {
+				//console.log(obj);
+				console.log("started comics");
+				for (var j = 0; j < obj.profile_url.length; j++) {
+					addComic(obj, j);
+				}
+				console.log("finished comics");
+			}
+		});
+	}
+}
+
+function addComic(obj, i) {
+	var chapter = obj.current_chapter[i].replace("</b>", "").replace("<b>", "").replace("Comics: ", "");
+	var comic = new Models.Comic({
+		url: obj.url[i],
+		comic_title: obj.comic_title[i],
+		archive_url: obj.archive_url[i],
+		profile_url: obj.profile_url[i],
+		current_chapter: chapter,
+		image: obj.image[i]
+	});
+
+	comic.save(function(err, com) {
+		console.log("save comic: " +err);
+		if (!err) {
+			addProfile(obj.profile_url[i], obj.comic_title[i], com._id);
+			addChapters(obj.archive_url[i], obj.comic_title[i], com._id);
+		}
+	});
+}
+
+function addProfile(data, title, id) {
+	console.log("add Profile called");
+
+	x(data, {
 		genre: ".authorinfo:nth-of-type(3) > span.info",
 		language: ".authorinfo:nth-of-type(2) > span.info:nth-child(2)",
 		author: ".authorname@html",
 		description: ".pccontent@html", // need to clean \n and \t
 		first_page: ".authorinfo:nth-of-type(7) > a@href"
 	})(function(err, obj) {
-		console.log(obj);
-		var list = [];
-		var description = obj.description.replace("/t", "").replace("/n", "");
-		res.json(obj);
-		for (var i = 0; i < obj.profile_url.length; i++) {
-			var profile = new Models.ComicProfile({
+		if (!err) {
+			//console.log(obj);
+			var description = obj.description.replace("/t", "").replace("/n", "");
+			var profile = new Models.Profile({
 				comic_id: id,
 				comic_title: title,
 				genre: obj.genre,
@@ -64,38 +81,78 @@ function randomA(index, data, title, id) {
 				description: obj.description,
 				first_page: obj.first_page
 			});
-			profile.save(function() {
-
+			//console.log(profile);
+			profile.save(function(err, profileObj) {
+				console.log("profile save");
+				console.log(err);
 			});
+		}
+	});
+}
+
+function addChapters(archive_url, title, id) {
+	console.log("add chapters called");
+	var date = moment().format("MM/DD/YYYY");
+	x(archive_url, {
+		page_name: ['a'],
+		page_url: ['a@href']
+	})(function(err, obj) {
+		//console.log("start");
+		//console.log("add chapter to " + title + " got " + err);
+		for (var i = 0; i < obj.page_name.length; i++) {
+			//console.log("loop");
+			var page_url = obj.page_url[i];
+			var page_name = obj.page_name[i];
+			var page_arr = obj.page_url[i].split('/');
+			//console.log(page_url);
+			if (typeof page_arr !== 'undefined' && page_arr.length > 1 && 
+				!isNaN(parseInt(page_arr[page_arr.length - 2]))) {
+				var page_number = parseInt(page_arr[page_arr.length - 2]); // does if check first to prevent possible error
+				//console.log(page_number);
+				var page = new Models.Page({
+					comic_id: id,
+					comic_title: title,
+					page_title: page_name,
+					page: page_number,
+					page_url: page_url,
+					date_added: date
+				});
+				//console.log(page);
+				page.save(function(err, pageObj) {
+					console.log("page save");
+					console.log(err);
+				});
+			}
 		}
 	});
 }
 
 router.get('/archive', function(req, res, next) {
 	var date = moment().format("MM/DD/YYYY");
-	x('http://inontheout.thecomicseries.com/archive/', {
-		chapter_name: ['a'],
-		chapter_url: ['a@href']
+	x('http://spaceronind.thecomicseries.com/archive/', {
+		page_name: ['a'],
+		page_url: ['a@href']
 	})(function(err, obj) {
 		console.log("start");
 		console.log(obj);
-		for (var i = 0; i < obj.chapter_name.length; i++) {
+		for (var i = 0; i < obj.page_name.length; i++) {
 			console.log("loop");
-			var chapter_url = obj.chapter_url[i];
-			var chapter_name = obj.chapter_name[i];
-			var chapter_arr = obj.chapter_url[i].split('/');
-			console.log(chapter_url);
-			if (typeof chapter_arr !== 'undefined' && chapter_arr.length > 1 && 
-				!isNaN(parseInt(chapter_arr[chapter_arr.length - 2]))) {
-				var chapter_number = parseInt(chapter_arr[chapter_arr.length - 2]);
-				console.log(chapter_number);
-				var chapter = new Models.Chapter({
-					comic_title: "http://thebloomsaga.thecomicseries.com/archive/",
-					chapter_title: chapter_name,
-					chapter: chapter_number,
-					chapter_url: chapter_url
+			var page_url = obj.page_url[i];
+			var page_name = obj.page_name[i];
+			var page_arr = obj.page_url[i].split('/');
+			console.log(page_url);
+			if (typeof page_arr !== 'undefined' && page_arr.length > 1 && 
+				!isNaN(parseInt(page_arr[page_arr.length - 2]))) {
+				var page_number = parseInt(page_arr[page_arr.length - 2]);
+				console.log(page_number);
+				var page = new Models.Page({
+					comic_title: "http://spaceronind.thecomicseries.com/archive/",
+					page_title: page_name,
+					page: page_number,
+					page_url: page_url,
+					date_added: date
 				});
-				chapter.save(function(err, comic) {
+				page.save(function(err, comic) {
 					console.log(err);
 				});
 			}
