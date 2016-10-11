3441553c0367437c9ee613c25223bba544859c7c
chang5CAL <ginka13@gmail.com>
2016-10-10 21:40:37 -0700

    added new modules

--- public/javascripts/app/app.module.ts
+++ public/javascripts/app/app.module.ts
@@ -1,9 +1,44 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent }   from './app.component';
+import { LatestComponent } from './app.latest';
+import { GenreComponent } from './app.genre';
+
+import { ComicService } from './comic.service';
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+  	BrowserModule,
+  	FormsModule,
+  	HttpModule,
+  	RouterModule.forRoot([
+  		{
+  			path: '',
+        redirectTo: '/home/1',
+        pathMatch: 'full'
+  		},
+  		{
+  			path: 'home/:page',
+  			component: LatestComponent
+  		},
+  		{
+  			path: 'random',
+  			component: GenreComponent
+  		}
+  	])
+  ],
+  declarations: [ 
+  	AppComponent,
+  	LatestComponent,
+  	GenreComponent
+  ],
+  providers: [
+  	ComicService,
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
