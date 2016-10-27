ca6eb430c256a26b3536690fb39574a0da23eb9f
Justin <ginka13@gmail.com>
2016-10-27 12:49:30 -0700

    Added code from genre to latest for template

--- public/javascripts/app/app.latest.ts
+++ public/javascripts/app/app.latest.ts
@@ -1,6 +1,23 @@
 import { Component } from '@angular/core';
+import { Comic } from './comic';
+import { ComicService } from './comic.service';
+
 @Component({
   selector: 'latest',
   templateUrl: 'javascripts/app/app.latest.html'
 })
-export class LatestComponent { }
+export class LatestComponent { 
+	comics: Comic[];
+	constructor(
+		private comicService: ComicService
+	) {}
+
+	getLatestComics(): void {
+		this.comicService
+			.getLatestComics(1)
+			.then(comics => {
+				console.log(comics);
+				this.comics = comics;
+			});
+	}
+}
