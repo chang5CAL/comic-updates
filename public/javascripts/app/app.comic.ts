4b250ef84a2d4bb6b7e9bdac0189befe97ae8774
Justin <ginka13@gmail.com>
2016-10-29 11:28:37 -0700

    Added comic pages for individual comics

--- public/javascripts/app/app.comic.ts
+++ public/javascripts/app/app.comic.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Comic } from './comic';
 import { ComicService } from './comic.service';
 @Component({
-  selector: 'genre',
+  selector: 'comic',
   templateUrl: 'javascripts/app/app.genre.html'
 })
 export class GenreComponent { 
